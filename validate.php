2a0a945dc782aebf7b8d7280118211e7907f8a28
CedricShimuli <shimulicedric@gmail.com>
2019-12-02 09:17:51 +0300

    first commit

---
+++ validate.php
@@ -0,0 +1,18 @@
+<?php
+
+  echo "Validate";
+  $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  $credentials = base64_encode('xr543NDN3oRBmx3j2XH1fR5S02LMW0q6:qDnh1S5A2XPPmwR7');
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
+  curl_setopt($curl, CURLOPT_HEADER, true);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  
+  $curl_response = curl_exec($curl);
+  
+  echo json_decode($curl_response);
+  
+  ?>
+  
